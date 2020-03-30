18010d9b7e6b103f0d2120f6e6891615c727163f
DzmitreyDanilau <dzmitrey.danilau@gmail.com>
2020-03-30 23:25:47 +0300

    Init dagger component inside project [Registration]

--- app/src/main/java/by/dzmitrey/danilau/muzhelpermessenger/ChatModuleApp.kt
+++ app/src/main/java/by/dzmitrey/danilau/muzhelpermessenger/ChatModuleApp.kt
@@ -1,12 +1,26 @@
 package by.dzmitrey.danilau.muzhelpermessenger
 
 import android.app.Application
+import by.dzmitrey.danilau.muzhelpermessenger.di.components.ApplicationComponent
+import by.dzmitrey.danilau.muzhelpermessenger.di.components.DaggerApplicationComponent
 import timber.log.Timber
 
-class ChatModuleApp : Application(){
+class ChatModuleApp : Application() {
+
+    val appComponent: ApplicationComponent by lazy {
+        initApplicationComponent()
+    }
 
     override fun onCreate() {
         super.onCreate()
+        plantDebugTree()
+    }
+
+    private fun plantDebugTree() {
         Timber.plant(Timber.DebugTree())
+    }
+
+    private fun initApplicationComponent() : ApplicationComponent {
+     return   DaggerApplicationComponent.create()
     }
 }
