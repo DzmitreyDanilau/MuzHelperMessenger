bc1000c5197d2d762aa80d18fdf822a7c7f9f3d5
Dzmitrey Danilay <dznitrey.danilau@gmail.com>
2020-04-01 00:26:15 +0300

    Add home package [Home]

---
+++ app/src/main/java/by/dzmitrey/danilau/muzhelpermessenger/home/presentation/HomeActivity.kt
@@ -0,0 +1,11 @@
+package by.dzmitrey.danilau.muzhelpermessenger.home.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
