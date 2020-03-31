630590707348331d88a6e7baff82974c179e34cc
Dzmitrey Danilay <dznitrey.danilau@gmail.com>
2020-04-01 00:24:16 +0300

    Add login package [Login]

---
+++ app/src/main/java/by/dzmitrey/danilau/muzhelpermessenger/login/presentation/AuthActivity.kt
@@ -0,0 +1,13 @@
+package by.dzmitrey.danilau.muzhelpermessenger.login.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import by.dzmitrey.danilau.muzhelpermessenger.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
